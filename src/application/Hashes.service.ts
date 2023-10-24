34a919abbe77a83b6eeef1575c8bee3c48cdaee7
“moviedo” <moviedo@konecta-grupo.com>
2023-10-24 17:03:04 -0300

    hashesRepo

--- src/application/Hashes.service.ts
+++ src/application/Hashes.service.ts
@@ -2,20 +2,13 @@ import {IHashesService} from "@domain/IHashesService";
 import {IHashesRepository} from "./interfases/IHashesRepository";
 
 export default class HashesService implements IHashesService {
+  constructor(private readonly hashesRepo: IHashesRepository) {}
 
-
-  constructor(private readonly HashesRepo: IHashesRepository) {
-      
-  }
- 
-
-  public async Set(key: string,field:string, value: string): Promise<string> {
-    return this.HashesRepo.Set(key,field,value);
+  public async Set(key: string, field: string, value: string): Promise<string> {
+    return this.hashesRepo.Set(key, field, value);
   }
 
-  public async Get(key: string,field:string): Promise<string> {
-    return this.HashesRepo.Get(key,field);
+  public async Get(key: string, field: string): Promise<string> {
+    return this.hashesRepo.Get(key, field);
   }
-  
-
 }
